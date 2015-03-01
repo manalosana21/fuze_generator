f5d5648385b28c8217208383dd2a60abb076b3b7
manalosana21 <manalosana21@yahoo.com>
2015-03-01 15:16:41 -0800

    redo generate code to simpler function

--- www/fuze_create_code.php
+++ www/fuze_create_code.php
@@ -6,12 +6,12 @@ good_connect();
 $enum_fields = array(); 
 $required_fields = array();
  //simulation
-//$enum_fields = array('enum_file_category_type' =>  array("1"=>"Active","0"=>"Disabled"),'enum_file_category_status' =>  array("0"=>"None","1"=>"Test"),);
-//$required_fields = array('file_category_name','file_category_type');
-//$table = 'file_category'; 
+$enum_fields = array('enum_file_category_type' =>  array("0"=>"None","1"=>"Test"));
+$required_fields = array('file_category_name','file_category_type');
+$table = 'file_category'; 
 
-$required_fields = array('location_contacts_name');
-$table = 'location_contacts'; 
+//$required_fields = array('location_contacts_name');
+//$table = 'location_contacts'; 
 
 //$required_fields = array('location_port_message_text');
 //$table = 'location_cx1'; 
